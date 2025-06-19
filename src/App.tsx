49324775c24863241484c089f739606950585f38
Felipe Afonso <fmunhozafonso@gmail.com>
2025-06-19 17:15:27 -0300

    feat: added storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 function App() {
-  return (
-    <h1 className="text-3xl font-bold">
-      Hi Mom
-    </h1>)
+	return (
+		<div>
+			<h1 className="text-3xl font-bold text-amber-50">Hi Mom</h1>
+		</div>
+	)
 }
 
 export default App
