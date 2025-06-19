0c4b3534cc12ea0b9b4108dc0ab0c4117d3c5108
Felipe Afonso <fmunhozafonso@gmail.com>
2025-06-19 19:01:45 -0300

    feat: integrated everything together

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
 	return (
 		<div>
-			<h1 className="text-3xl font-bold text-amber-50">Hi Mom</h1>
+			<h1 className="text-3xl font-bold">Hi Mom</h1>
+			<Button>Test</Button>
 		</div>
 	)
 }
