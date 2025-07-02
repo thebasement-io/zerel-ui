b26b1c3779a600577d46e9fba0476ff3e50cda48
Felipe Afonso <fmunhozafonso@gmail.com>
2025-07-02 18:30:31 -0300

    feat: sidebar story

---
+++ src/stories/Sidebar.stories.tsx
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider } from '@/components/ui/sidebar'
+import '@/index.css'
+import { Calendar, Home, Inbox, Search, Settings } from 'lucide-react'
+
+const meta = {
+	title: 'UI/Sidebar',
+	component: Sidebar,
+} satisfies Meta<typeof Sidebar>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+
+export const Default: Story = {
+	render: () => (
+		<SidebarProvider>
+			<Sidebar>
+				<SidebarContent>
+					<SidebarGroup>
+						<SidebarGroupLabel>Application</SidebarGroupLabel>
+						<SidebarGroupContent>
+							<SidebarMenu>
+								{items.map((item) => (
+									<SidebarMenuItem key={item.title}>
+										<SidebarMenuButton asChild>
+											<a href={item.url}>
+												<item.icon />
+												<span>{item.title}</span>
+											</a>
+										</SidebarMenuButton>
+									</SidebarMenuItem>
+								))}
+							</SidebarMenu>
+						</SidebarGroupContent>
+					</SidebarGroup>
+				</SidebarContent>
+			</Sidebar>
+		</SidebarProvider>
+	),
+}
+const items = [
+	{
+		title: "Home",
+		url: "#",
+		icon: Home,
+	},
+	{
+		title: "Inbox",
+		url: "#",
+		icon: Inbox,
+	},
+	{
+		title: "Calendar",
+		url: "#",
+		icon: Calendar,
+	},
+	{
+		title: "Search",
+		url: "#",
+		icon: Search,
+	},
+	{
+		title: "Settings",
+		url: "#",
+		icon: Settings,
+	},
+]
