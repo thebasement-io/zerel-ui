8ae36c7866ed658c2406054e395504c0a0930f0a
Felipe Afonso <fmunhozafonso@gmail.com>
2025-07-03 18:22:29 -0300

    fix: dark mode override

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -4,7 +4,7 @@ function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
 	return (
 		<div
 			data-slot="skeleton"
-			className={cn('bg-neutral-100 dark:bg-neutral-900 animate-pulse rounded-md', className)}
+			className={cn('bg-neutral-100 animate-pulse rounded-md', className)}
 			{...props}
 		/>
 	)
