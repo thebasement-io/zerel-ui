93510f8d7dcda560ed7fcd7868e311c70c7afb1e
Felipe Afonso <fmunhozafonso@gmail.com>
2025-07-23 16:44:14 -0300

    fix: sonner

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,9 +10,9 @@ const Toaster = ({ ...props }: ToasterProps) => {
             className="toaster group"
             style={
                 {
-                    '--normal-bg': 'var(--popover)',
-                    '--normal-text': 'var(--popover-foreground)',
-                    '--normal-border': 'var(--border)',
+                    '--normal-bg': 'var(--color-popover)',
+                    '--normal-text': 'var(--color-popover-foreground)',
+                    '--normal-border': 'var(--color-border)',
                 } as React.CSSProperties
             }
             {...props}
