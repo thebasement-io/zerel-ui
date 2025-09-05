bc73d91ff1b6f633fc88d2125460fb41a466e7ff
Felipe Afonso <fmunhozafonso@gmail.com>
2025-09-05 18:10:53 -0300

    fix: dark color issues

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,13 +1,16 @@
 import { cn } from '@/lib/utils'
 
 function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
-	return (
-		<div
-			data-slot="skeleton"
-			className={cn('bg-neutral-100 animate-pulse rounded-md', className)}
-			{...props}
-		/>
-	)
+    return (
+        <div
+            data-slot="skeleton"
+            className={cn(
+                'bg-foreground/13 animate-pulse rounded-md',
+                className,
+            )}
+            {...props}
+        />
+    )
 }
 
 export { Skeleton }
