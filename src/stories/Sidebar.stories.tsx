7a92ff2fc8b573098d537ff0994028abb16d7865
Felipe Afonso <fmunhozafonso@gmail.com>
2025-09-05 18:37:25 -0300

    chore: format cleanup

--- src/stories/Sidebar.stories.tsx
+++ src/stories/Sidebar.stories.tsx
@@ -1,69 +1,81 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider } from '@/components/ui/sidebar'
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarProvider,
+} from '@/components/ui/sidebar'
 import '@/index.css'
 import { Calendar, Home, Inbox, Search, Settings } from 'lucide-react'
 
 const meta = {
-	title: 'UI/Sidebar',
-	component: Sidebar,
+    title: 'UI/Sidebar',
+    component: Sidebar,
 } satisfies Meta<typeof Sidebar>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-
 export const Default: Story = {
-	render: () => (
-		<SidebarProvider>
-			<Sidebar>
-				<SidebarContent>
-					<SidebarGroup>
-						<SidebarGroupLabel>Application</SidebarGroupLabel>
-						<SidebarGroupContent>
-							<SidebarMenu>
-								{items.map((item) => (
-									<SidebarMenuItem key={item.title}>
-										<SidebarMenuButton isActive={item.active} asChild>
-											<a href={item.url}>
-												<item.icon />
-												<span>{item.title}</span>
-											</a>
-										</SidebarMenuButton>
-									</SidebarMenuItem>
-								))}
-							</SidebarMenu>
-						</SidebarGroupContent>
-					</SidebarGroup>
-				</SidebarContent>
-			</Sidebar>
-		</SidebarProvider>
-	),
+    render: () => (
+        <SidebarProvider>
+            <Sidebar>
+                <SidebarContent>
+                    <SidebarGroup>
+                        <SidebarGroupLabel>Application</SidebarGroupLabel>
+                        <SidebarGroupContent>
+                            <SidebarMenu>
+                                {items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton
+                                            isActive={item.active}
+                                            asChild
+                                        >
+                                            <a href={item.url}>
+                                                <item.icon />
+                                                <span>{item.title}</span>
+                                            </a>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
+                </SidebarContent>
+            </Sidebar>
+        </SidebarProvider>
+    ),
 }
 const items = [
-	{
-		title: "Home",
-		url: "#",
-		icon: Home,
-	},
-	{
-		title: "Inbox",
-		url: "#",
-		icon: Inbox,
-		active: true
-	},
-	{
-		title: "Calendar",
-		url: "#",
-		icon: Calendar,
-	},
-	{
-		title: "Search",
-		url: "#",
-		icon: Search,
-	},
-	{
-		title: "Settings",
-		url: "#",
-		icon: Settings,
-	},
+    {
+        title: 'Home',
+        url: '#',
+        icon: Home,
+    },
+    {
+        title: 'Inbox',
+        url: '#',
+        icon: Inbox,
+        active: true,
+    },
+    {
+        title: 'Calendar',
+        url: '#',
+        icon: Calendar,
+    },
+    {
+        title: 'Search',
+        url: '#',
+        icon: Search,
+    },
+    {
+        title: 'Settings',
+        url: '#',
+        icon: Settings,
+    },
 ]
